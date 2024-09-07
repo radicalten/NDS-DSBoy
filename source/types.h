eb437cf1ff240983d773edc538b057cc62f4e266
radicalten <79894181+radicalten@users.noreply.github.com>
2024-09-07 12:33:51 -0500

    Update types.h

--- source/types.h
+++ source/types.h
@@ -1,15 +1,15 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
-typedef unsigned char           u8;
-typedef unsigned short int      u16;
-typedef unsigned int            u32;
-typedef unsigned long long int  u64;
+typedef uint8_t                  u8;
+typedef uint16_t                u16;
+typedef uint32_t                u32;
+typedef uint64_t                u64;
 
-typedef signed char             s8;
-typedef signed short int        s16;
-typedef signed int              s32;
-typedef signed long long int    s64;
+typedef int8_t                   s8;
+typedef int16_t                 s16;
+typedef int32_t                 s32;
+typedef int64_t                 s64;
 
 typedef volatile u8          vu8;
 typedef volatile u16         vu16;
